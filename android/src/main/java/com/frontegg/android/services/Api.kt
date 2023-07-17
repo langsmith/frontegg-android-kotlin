af7db58026c723f2bab8a5ce120312db0497057b
David Antoon <david@frontegg.com>
2023-07-17 17:29:22 +0300

    add try catch to api requests

--- android/src/main/java/com/frontegg/android/services/Api.kt
+++ android/src/main/java/com/frontegg/android/services/Api.kt
@@ -1,5 +1,6 @@
 package com.frontegg.android.services
 
+import android.util.Log
 import com.frontegg.android.models.AuthResponse
 import com.frontegg.android.models.User
 import com.frontegg.android.utils.ApiConstants
@@ -19,14 +20,12 @@ import java.io.IOException
 open class Api(
     private var baseUrl: String,
     private var clientId: String,
-    private var credentialManager: CredentialManager
+    private var credentialManager: CredentialManager,
+    private var httpClient: OkHttpClient = OkHttpClient()
 ) {
-    private var httpClient: OkHttpClient
-
-    init {
-        this.httpClient = OkHttpClient()
+    companion object {
+        private val TAG = Api::class.java.simpleName
     }
-
 
     private fun prepareHeaders(additionalHeaders: Map<String, String> = mapOf()): Headers {
 
@@ -80,35 +79,40 @@ open class Api(
         return this.httpClient.newCall(request)
     }
 
-    @Throws(IllegalArgumentException::class, IOException::class)
+    @Throws(IllegalArgumentException::class)
     public fun me(): User? {
-        val call = buildGetRequest(ApiConstants.me)
-        val response = call.execute()
-        if (response.isSuccessful) {
-            return Gson().fromJson(response.body!!.string(), User::class.java)
-        }
+        try {
+            val call = buildGetRequest(ApiConstants.me)
+            val response = call.execute()
+            if (response.isSuccessful) {
+                return Gson().fromJson(response.body!!.string(), User::class.java)
+            }
 
+        } catch (e: IOException) {
+            Log.e(TAG, "me() ${e.message}", e)
+        }
         return null
     }
 
-    @Throws(IllegalArgumentException::class, IOException::class)
+    @Throws(IllegalArgumentException::class)
     public fun refreshToken(refreshToken: String): AuthResponse? {
-
-
         val body = JsonObject()
         body.addProperty("grant_type", "refresh_token")
         body.addProperty("refresh_token", refreshToken)
-
-        val call = buildPostRequest(ApiConstants.refreshToken, body)
-        val response = call.execute()
-        if (response.isSuccessful) {
-            return Gson().fromJson(response.body!!.string(), AuthResponse::class.java)
+        try {
+            val call = buildPostRequest(ApiConstants.refreshToken, body)
+            val response = call.execute()
+            if (response.isSuccessful) {
+                return Gson().fromJson(response.body!!.string(), AuthResponse::class.java)
+            }
+        } catch (e: IOException) {
+            Log.e(TAG, "refreshToken() ${e.message}", e)
         }
         return null
     }
 
 
-    @Throws(IllegalArgumentException::class, IOException::class)
+    @Throws(IllegalArgumentException::class)
     public fun exchangeToken(
         code: String,
         redirectUrl: String,
@@ -121,11 +125,14 @@ open class Api(
         body.addProperty("code_verifier", codeVerifier)
         body.addProperty("grant_type", "authorization_code")
 
-
-        val call = buildPostRequest(ApiConstants.exchangeToken, body)
-        val response = call.execute()
-        if (response.isSuccessful) {
-            return Gson().fromJson(response.body!!.string(), AuthResponse::class.java)
+        try {
+            val call = buildPostRequest(ApiConstants.exchangeToken, body)
+            val response = call.execute()
+            if (response.isSuccessful) {
+                return Gson().fromJson(response.body!!.string(), AuthResponse::class.java)
+            }
+        } catch (e: IOException) {
+            Log.e(TAG, "exchangeToken() ${e.message}", e)
         }
         return null
     }
@@ -134,12 +141,16 @@ open class Api(
         val refreshToken = this.credentialManager.getOrNull(CredentialKeys.ACCESS_TOKEN)
 
         if (refreshToken != null) {
-            val call = buildPostRequest(
-                ApiConstants.logout, JsonObject(), mapOf(
-                    Pair("fe_refresh_$clientId", refreshToken)
+            try {
+                val call = buildPostRequest(
+                    ApiConstants.logout, JsonObject(), mapOf(
+                        Pair("fe_refresh_$clientId", refreshToken)
+                    )
                 )
-            )
-            call.execute()
+                call.execute()
+            } catch (e: IOException) {
+                Log.e(TAG, "logout() ${e.message}", e)
+            }
         }
     }
 }
